60ef1f8457ac0d79bb0a2037a07378c5c291d011
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-03-14 23:04:16 -0400

    established and verified connection to database

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-//Dependencies===================//
+//Dependencies//
 const inquirer = require("inquirer")
 const mysql = require("mysql")
 const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
     user: "root",
     password: "@Tuesday12",
+    port: 3306,
     database: "employee_trackerDB"
   });
+  
+  connection.connect(function(err) {
+    if (err) throw err
+    //listOptions();
+});
